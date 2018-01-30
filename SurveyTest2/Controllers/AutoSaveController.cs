3be69167358cdb8ee07a4851354b56486899c817
Jessica <jessicahuangtfg@gmail.com>
2018-01-30 23:54:10 +0800

    Input_Front

---
+++ SurveyTest2/Controllers/AutoSaveController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SurveyTest2.Controllers
+{
+    public class AutoSaveController : Controller
+    {
+        // GET: InputTest
+        public ActionResult InputTest()
+        {
+            return View();
+        }
+    }
+}
