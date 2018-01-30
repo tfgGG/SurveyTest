59e1af6f4609016fe6c574a783e909cc526ab6bb
Jessica <jessicahuangtfg@gmail.com>
2018-01-30 16:43:13 +0800

    加入專案檔案。

---
+++ SurveyTest2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SurveyTest2.Startup))]
+namespace SurveyTest2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
